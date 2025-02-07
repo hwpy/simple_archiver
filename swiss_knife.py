f903b69b82a2f0e1fa33cae99b0bfe140744109c
hwpy <photodvfusr1@gmail.com>
2025-02-08 02:33:53 +1000

    mvc перемещены в каталог converter

--- swiss_knife.py
+++ swiss_knife.py
@@ -6,9 +6,9 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
 from config.swiss_knife_config import SwissKnifeConfig
-from controller.swiss_knife_controller import SwissKnifeController
-from model.archiver_manager import ArchiveManager
-from view.swiss_knife_view import SwissKnifeView
+from converter.controller.swiss_knife_controller import SwissKnifeController
+from converter.model.archiver_manager import ArchiveManager
+from converter.view.swiss_knife_view import SwissKnifeView
 
 
 class SwissKnifeApp:
